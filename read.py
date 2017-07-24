97508e04477d31a96a6e6d099af89784785d2da1
HDRorz <ab250985201@gmail.com>
2017-07-24 22:40:41 +0800

    我不知道我上次改了些什么
    ???

--- read.py
+++ read.py
@@ -2,6 +2,7 @@
 
 import os
 import copy
+import datetime
 
 from openpyxl import Workbook
 from openpyxl import load_workbook
@@ -36,7 +37,14 @@ class Reader(object):
         for no in buyno[1:]:
             row_num += 1
             row = rows[row_num]
-            temp = {'buyno': no.value, 'row': self.tran(row)}
+            no_value = no.value
+            if no_value is None:
+                no_value = ''
+            temp = {'buyno': no_value.split(',')[0].strip(),
+                    'row': self.tran(row)}
+            temp['name'] = temp['row'][3].strip()
+            temp['time'] = temp['row'][5]
+            temp['value'] = temp['row'][7]
             buylist.append(temp)
 
         payno = self.pay_sheet.columns.next()
@@ -49,8 +57,18 @@ class Reader(object):
         for no in payno[1:]:
             row_num += 1
             row = rows[row_num]
-            temp = {'payno': no.value, 'row': self.tran(row)}
+            no_value = no.value
+            if no_value is None:
+                no_value = ''
+            temp = {'payno': no_value.split(',')[0].strip(),
+                    'row': self.tran(row)}
+            temp['name'] = temp['row'][5].strip()
+            temp['time'] = temp['row'][3]
+            temp['value'] = temp['row'][9] - temp['row'][13]
             paylist.append(temp)
+
+        buylist = self.distinct(buylist)
+        paylist = self.distinct(paylist)
 
         buy_emptyrow = copy.copy(self.buy_title_row)
         for i in range(0, len(buy_emptyrow)):
@@ -65,18 +83,20 @@ class Reader(object):
         right_list = []
 
         for buy in buylist:
-            temp = {'buy': buy, 'pay': {'payno': '', 'row': pay_emptyrow}}
+            temp = {'buy': buy, 'pay': {'payno': '', 'name': '', 'time': '', 'value': '', 'row': pay_emptyrow}}
             for pay in paylist:
-                if buy['buyno'].__class__ == pay['payno'].__class__ and buy['buyno'].strip() == pay['payno'].strip():
+                if buy['buyno'].__class__ == pay['payno'].__class__ and \
+                                buy['buyno'] == pay['payno']:
                     temp['pay'] = pay
                     inner_list.append(temp)
                     break
             left_list.append(temp)
 
         for pay in paylist:
-            temp = {'buy': {'buy': '', 'row': buy_emptyrow}, 'pay': pay}
+            temp = {'buy': {'buy': '', 'name': '', 'time': '', 'value': '', 'row': buy_emptyrow}, 'pay': pay}
             for buy in buylist:
-                if buy['buyno'].__class__ == pay['payno'].__class__ and buy['buyno'].strip() == pay['payno'].strip():
+                if buy['buyno'].__class__ == pay['payno'].__class__ and \
+                                buy['buyno'] == pay['payno']:
                     temp['pay'] = pay
                     break
             right_list.append(temp)
@@ -84,6 +104,45 @@ class Reader(object):
         self.save('out' + str(index) + '.xlsx', '订单与支付都有'.decode('utf8'), inner_list)
         self.save('out' + str(index) + '.xlsx', '所有订单+支付'.decode('utf8'), left_list)
         self.save('out' + str(index) + '.xlsx', '订单+所有支付'.decode('utf8'), right_list)
+
+    def distinct(self, datalist):
+        newlist = []
+        cur_row_num = 0
+        total_len = datalist.__len__()
+        while True:
+            row = datalist[cur_row_num]
+            newlist.append(row)
+            for row_num in range(total_len - 1, cur_row_num, -1):
+                rowdata = datalist[row_num]
+                if row['name'].__class__ == rowdata['name'].__class__ \
+                        and row['name'].strip() == rowdata['name'].strip() \
+                        and row['time'].__class__ == rowdata['time'].__class__:
+                    try:
+                        if row['time'].__class__ == unicode \
+                                and abs((datetime.datetime.strptime(row['time'], '%Y-%m-%d %H:%M:%S') -
+                                             datetime.datetime.strptime(rowdata['time'],
+                                                                        '%Y-%m-%d %H:%M:%S')).days) <= 1:
+                            try:
+                                row['value'] += rowdata['value']
+                            except Exception, e:
+                                pass
+                            datalist.remove(rowdata)
+                        if row['time'].__class__ == datetime \
+                                and abs((row['time'] - rowdata['time']).days) <= 1:
+                            try:
+                                row['value'] += rowdata['value']
+                            except Exception, e:
+                                pass
+                            datalist.remove(rowdata)
+                    except Exception, e:
+                        pass
+
+            cur_row_num += 1
+            total_len = datalist.__len__()
+            if cur_row_num >= total_len:
+                break
+
+        return newlist
 
     def save(self, filename, sheetname, outlist):
         if os.path.exists(filename):
@@ -98,10 +157,13 @@ class Reader(object):
         for title in self.buy_title_row:
             sheet.cell(column=col_i, row=row_i).value = title
             col_i += 1
+        sheet.cell(column=col_i, row=row_i).value = '订单总额'
+        col_i += 1
 
         for title in self.pay_title_row:
             sheet.cell(column=col_i, row=row_i).value = title
             col_i += 1
+        sheet.cell(column=col_i, row=row_i).value = '支付总额'
 
         col_i = 1
         row_i += 1
@@ -110,10 +172,13 @@ class Reader(object):
             for ce in item['buy']['row']:
                 sheet.cell(column=col_i, row=row_i).value = ce
                 col_i += 1
+            sheet.cell(column=col_i, row=row_i).value = item['buy']['value']
+            col_i += 1
 
             for ce in item['pay']['row']:
                 sheet.cell(column=col_i, row=row_i).value = ce
                 col_i += 1
+            sheet.cell(column=col_i, row=row_i).value = item['pay']['value']
 
             col_i = 1
             row_i += 1
